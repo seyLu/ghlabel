b02c4af75f1084595a2283a4ad6f6568c06480f2
seylu <98249191+seyLu@users.noreply.github.com>
2023-08-27 16:31:31 +0800

    feat: add load_label and create_label methods

--- scripts/setup_issue_label.py
+++ scripts/setup_issue_label.py
@@ -13,11 +13,24 @@ __version__ = "0.0.1"
 __maintainer__ = "seyLu"
 __status__ = "Prototype"
 
+import logging
 import os
+import sys
 from dataclasses import dataclass
 from logging.config import fileConfig
 
+import requests
+import yaml
 from dotenv import load_dotenv
+from requests.models import Response
+
+load_dotenv()
+
+
+@dataclass(frozen=True)
+class BasePath:
+    CWD: str = os.path.dirname(__file__)
+    CONFIG: str = "config"
 
 
 @dataclass(frozen=True)
@@ -28,28 +41,67 @@ class GithubConfig:
     REPO_NAME: str = os.getenv("GITHUB_REPO_NAME")
 
 
+@dataclass(frozen=True)
+class LabelFile:
+    _BASE_FILE: str = os.path.join(BasePath.CWD, "..", BasePath.CONFIG, "labels")
+    YAML: str = f"{_BASE_FILE}.yaml"
+    JSON: str = f"{_BASE_FILE}.json"
+
+
 class GithubIssueLabel:
-    def __init__(self):
-        self._url: str = f"https//api.github.com/repos/{GithubConfig.REPO_OWNER}/{GithubConfig.REPO_NAME}/labels"
-        self._headers: dict = {
+    def __init__(self) -> None:
+        self._url: str = f"https://api.github.com/repos/{GithubConfig.REPO_OWNER}/{GithubConfig.REPO_NAME}/labels"
+        self._headers: dict[str, str] = {
             "Authorization": f"Bearer {GithubConfig.PERSONAL_ACCESS_TOKEN}",
             "Accept": "application/vnd.github+json",
             "X-GitHub-Api-Version": "2022-11-28",
         }
-        self._labels: dict = self._load_labels()
+        self._labels: list[dict[str, str]] = self._load_labels()
 
-    def _load_labels() -> dict:
-        pass
+    def _load_labels(self) -> list[dict[str, str]]:
+        labels: list[dict[str, str]]
+        label_file: str = ""
 
-    def create_labels():
-        pass
+        if os.path.isfile(LabelFile.YAML):
+            logging.info(
+                f"Found {LabelFile.YAML.split('../')[1]}. Loading labels from yaml config."
+            )
+            label_file = LabelFile.YAML
+        elif os.path.isfile(LabelFile.JSON):
+            logging.info(
+                f"Found {LabelFile.JSON.split('../')[1]}. Loading labels from json config."
+            )
+            label_file = LabelFile.JSON
+        else:
+            logging.error("No Yaml or JSON config file for labels.")
+            sys.exit()
+
+        with open(label_file, "r") as f:
+            if label_file == LabelFile.YAML:
+                labels = yaml.safe_load(f)
+
+        return labels
+
+    def create_labels(self) -> None:
+        for label in self._labels:
+            res: Response = requests.post(
+                url=self._url,
+                headers=self._headers,
+                json=label,
+            )
+
+            if res.status_code == 201:
+                logging.info(f"Label {label['name']} created successfully.")
+            else:
+                logging.error(f"Failed to create label {label['name']}.")
 
 
 def main() -> None:
-    pass
+    github_issue_label = GithubIssueLabel()
+    github_issue_label.create_labels()
+    logging.info("Label creation process completed.")
 
 
 if __name__ == "__main__":
-    load_dotenv()
     fileConfig("logging.ini")
     main()
