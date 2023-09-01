6ad04a8758b4d53f882d919baac723dcb0d4b8bf
seylu <98249191+seyLu@users.noreply.github.com>
2023-09-01 18:01:34 +0800

    chore: use __version__ for script version

--- scripts/_dump_labels.py
+++ scripts/_dump_labels.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
         "--version",
         "-v",
         action="version",
-        version="%(prog)s 0.0.1",
+        version=f"%(prog)s {__version__}",
     )
     parser.add_argument(
         "--ext",
