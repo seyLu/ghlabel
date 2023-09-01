5dbfdb5e4491e011041eb4eca9feac942d8e6ec0
seylu <98249191+seyLu@users.noreply.github.com>
2023-09-01 13:36:21 +0800

    feat: split to multiple label files and json dump

--- scripts/_create_label_config.py
+++ scripts/_create_label_config.py
@@ -13,82 +13,109 @@ __version__ = "0.0.1"
 __maintainer__ = "seyLu"
 __status__ = "Prototype"
 
+import json
+import logging
 import os
+from dataclasses import dataclass
 from logging.config import fileConfig
 
 import yaml
 
-CONFIG_PATH: str = "config"
-LABELS_FILE: str = os.path.join(CONFIG_PATH, "labels")
-LABELS_EXT: str = "yaml"
-LABELS: list[dict[str, str]] = [
-    # ----- Priority -----#
-    {
-        "name": "Priority: Critical",
-        "color": "#7c0a02",
-    },
-    {
-        "name": "Priority: High",
-        "color": "#b22222",
-    },
-    {
-        "name": "Priority: Medium",
-        "color": "#e88a1a",
-    },
-    {
-        "name": "Priority: Low",
-        "color": "#f1bc31",
-    },
-    # --------------------#
-    # ------- Type -------#
-    {
-        "name": "Type: Bug",
-        "color": "#d73a4a",
-        "description": "Something isn't working.",
-    },
-    {
-        "name": "Type: Documentation",
-        "color": "#a2eeef",
-        "description": "Improvements or additions to documentation.",
-    },
-    {
-        "name": "Type: Feature Request",
-        "color": "#e88a1a",
-        "description": "Issue describes a feature or enhancement we'd like to implement.",
-    },
-    {
-        "name": "Type: Question",
-        "color": "#d876e3",
-        "description": "This issue doesn't require code. A question needs an answer.",
-    },
-    {
-        "name": "Type: Refactor/Clean-up",
-        "color": "#a0855b",
-        "description": "Issues related to reorganization/clean-up of data or code (e.g. for maintainability).",
-    },
-    {
-        "name": "Type: Suggestion",
-        "color": "#ac8daf",
-    }
-    # --------------------#
-]
+fileConfig("logging.ini")
+
+
+def initialize_dir(dir: str) -> None:
+    logging.info(f"Initializing {dir} dir.")
+    for filename in os.listdir(dir):
+        file_path: str = os.path.join(dir, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+
+
+@dataclass(frozen=True)
+class LABELS:
+    REMOVE_DEFAULT: tuple[str, ...] = (
+        "bug",
+        "dependencies",
+        "documentation",
+        "enhancement",
+        "github_actions",
+        "question",
+    )
+    PRIORITY: tuple[dict[str, str], ...] = (
+        {
+            "name": "Priority: Critical",
+            "color": "#7c0a02",
+        },
+        {
+            "name": "Priority: High",
+            "color": "#b22222",
+        },
+        {
+            "name": "Priority: Medium",
+            "color": "#e88a1a",
+        },
+        {
+            "name": "Priority: Low",
+            "color": "#f1bc31",
+        },
+    )
+    TYPE: tuple[dict[str, str], ...] = (
+        {
+            "name": "Type: Bug",
+            "color": "#d73a4a",
+            "description": "Something isn't working.",
+        },
+        {
+            "name": "Type: Documentation",
+            "color": "#a2eeef",
+            "description": "Improvements or additions to documentation.",
+        },
+        {
+            "name": "Type: Feature Request",
+            "color": "#e88a1a",
+            "description": "Issue describes a feature or enhancement we'd like to implement.",
+        },
+        {
+            "name": "Type: Question",
+            "color": "#d876e3",
+            "description": "This issue doesn't require code. A question needs an answer.",
+        },
+        {
+            "name": "Type: Refactor/Clean-up",
+            "color": "#a0855b",
+            "description": "Issues related to reorganization/clean-up of data or code (e.g. for maintainability).",
+        },
+        {
+            "name": "Type: Suggestion",
+            "color": "#ac8daf",
+        },
+    )
 
 
 def main() -> None:
-    labels_file: str = f"{LABELS_FILE}.{LABELS_EXT}"
-    os.makedirs(os.path.dirname(labels_file), exist_ok=True)
+    EXT: str = "yaml"
+    LABELS_PATH: str = "labels"
 
-    with open(labels_file, "w+") as f:
-        print(
-            yaml.dump(
-                data=LABELS,
-                default_flow_style=False,
-                sort_keys=False,
-            ),
-            file=f,
-        )
+    initialize_dir(LABELS_PATH)
+
+    for field in LABELS.__dataclass_fields__:
+        labels = getattr(LABELS, field)
+        labels_file = os.path.join(LABELS_PATH, f"{field.lower()}_labels.{EXT}")
+
+        with open(labels_file, "w+") as f:
+            if EXT == "yaml":
+                print(
+                    yaml.dump(
+                        data=list(labels),
+                        default_flow_style=False,
+                        sort_keys=False,
+                    ),
+                    file=f,
+                )
+            elif EXT == "json":
+                json.dump(labels, f, indent=2)
 
 
 if __name__ == "__main__":
-    fileConfig("logging.ini")
     main()
