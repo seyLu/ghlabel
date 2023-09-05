bac092f0007032f81bb203be396ffa1044009c7f
seylu <98249191+seyLu@users.noreply.github.com>
2023-09-05 13:27:02 +0800

    learn: terminating

--- cli.py
+++ cli.py
@@ -13,9 +13,28 @@ __status__ = "Prototype"
 
 import typer
 
+existing_usernames = ["rick", "morty"]
 
-def main(name: str) -> None:
-    typer.secho(f"Welcome here {name}.", fg=typer.colors.CYAN)
+
+def maybe_create_user(username: str) -> None:
+    if username == "root":
+        print("The root user is reserved")
+        raise typer.Abort()
+    elif username in existing_usernames:
+        print("The user already exists")
+        raise typer.Exit(code=1)
+    else:
+        print(f"User created: {username}")
+
+
+def send_new_user_notification(username: str) -> None:
+    # Somehow send a notification here for the new user, maybe an email
+    print(f"Notification sent for new user: {username}")
+
+
+def main(username: str) -> None:
+    maybe_create_user(username=username)
+    send_new_user_notification(username=username)
 
 
 if __name__ == "__main__":
